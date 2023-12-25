d173f26a3eea69683255a3960b4b4d9df20b23a9
Fredrick Bernard Onduru <fredrickbernardonduru@gmail.com>
2023-12-25 16:59:13 +0300

    First change with several errors

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+
+
+import React from 'react';
+import {Container } from 'reactstrap';
+
+const Header = () =>
+{
+    return <section>
+        <Container>
+            <div className="naviation">#</div>
+        </Container>
+    </section>
+};
+export default Header;
